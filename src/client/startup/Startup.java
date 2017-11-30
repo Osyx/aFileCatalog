3f7d94bf77f09b30eb204070671f487b1e57bce7
Tiberians <andsj@kth.se>
2017-11-30 17:39:26 +0100

    Started on RMI testing

--- src/client/startup/Startup.java
+++ src/client/startup/Startup.java
@@ -1,4 +1,16 @@
 package client.startup;
 
+import java.rmi.RemoteException;
+import client.view.View;
+import
+
 public class Startup {
+    public static void main(String[] args) {
+        try {
+            new View();
+        } catch (RemoteException ex) {
+            System.out.println("Could not start client.");
+        }
+    }
+
 }
--- src/client/view/View.java
+++ src/client/view/View.java
@@ -1,4 +1,30 @@
 package client.view;
 
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+import common.ClientReacher;
+import common.Hello;
+
 public class View {
+
+    private final ClientReacher remoteObject;
+
+
+    public View() throws RemoteException {
+        remoteObject = new Output();
+    }
+
+    private class Output extends UnicastRemoteObject implements ClientReacher{
+        public Output() throws RemoteException{
+
+        }
+
+        @Override
+        public void receivedMessage(String message) {
+            System.out.println(message);
+        }
+    }
 }
+
+
